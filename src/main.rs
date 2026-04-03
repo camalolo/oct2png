68a09ca73d04caf3f351966bdb273784d41d5694
Laurent Camarasa <camalolo@gmail.com>
2026-04-03 17:31:48 +0800

    Update usage message format

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     if args.contains(&"--help".into()) {
         println!("oct2png - Decode base64-encoded PNG data from stdin and save to a file");
         println!();
-        println!("Usage: echo '<data>base64,<base64_data>' | oct2png <output_path>");
+        println!("Usage: echo <tool_output> | oct2png <output_file>.png");
         process::exit(0);
     }
 
     if args.len() != 2 {
-        eprintln!("Usage: echo '<data>base64,<base64_data>' | oct2png <output_path>");
+        eprintln!("Usage: echo <tool_output> | oct2png <output_file>.png");
         process::exit(1);
     }
 
